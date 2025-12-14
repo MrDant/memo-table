0d6677694ec2b75cafb07860a146c554bbb78b0c
Mael DANT <mael.dantuono@gmail.com>
2025-12-14 15:33:03 +0100

    V2.1
    add install button

--- app.vue
+++ app.vue
@@ -11,21 +11,4 @@
 </template>
 
 <script setup>
-// Dans un composant ou un plugin
-if (window.matchMedia('(display-mode: browser)').matches) {
-  window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    const installButton = document.createElement('button');
-    installButton.textContent = 'Installer l\'app';
-    installButton.style.position = 'fixed';
-    installButton.style.bottom = '20px';
-    installButton.style.right = '20px';
-    installButton.style.zIndex = '9999';
-    installButton.addEventListener('click', () => {
-      e.prompt();
-    });
-    document.body.appendChild(installButton);
-  });
-}
-
 </script>
--- plugins/pwa.client.ts
+++ plugins/pwa.client.ts
@@ -4,12 +4,26 @@ export default defineNuxtPlugin(() => {
       window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js')
           .then((registration) => {
-
           })
           .catch((error) => {
             console.error('ServiceWorker registration failed:', error);
           });
       });
     }
+    if (window.matchMedia('(display-mode: browser)').matches) {
+        window.addEventListener('beforeinstallprompt', (e) => {
+          e.preventDefault();
+          const installButton = document.createElement('button');
+          installButton.textContent = 'Installer l\'app';
+          installButton.style.position = 'fixed';
+          installButton.style.bottom = '20px';
+          installButton.style.right = '20px';
+          installButton.style.zIndex = '9999';
+          installButton.addEventListener('click', () => {
+            e.prompt();
+          });
+          document.body.appendChild(installButton);
+        });
+      }
   });
   
